37672200c7bfc17e6bde6c380d889cd097f4c1be
Luiz Felipe <luizfrc.9@gmail.com>
2024-06-13 19:45:30 -0300

    FEAT: Implementação dos saves de training

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+#model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO('runs/detect/train18/weights/best.pt')
 
 # Use the model
-results = model.train(data="config.yaml", epochs=12)  # train the model
+results = model.train(data="config.yaml", epochs=13, save = True, save_period = 3)  # train the model
